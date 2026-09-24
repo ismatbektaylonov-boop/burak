cff5dfa9461760fb5a88c0a20b507665cc61dfae
ismatbektaylonov <ismatbektaylonov@gmail.com>
2026-09-24 16:53:05 +0900

    fixed

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import mongoose from 'mongoose'
 import app from './app'
 
 mongoose
-	.connect(process.env.MONGO_URI as string, {})
+	.connect(process.env.MONGO_URL as string, {})
 	.then(data => {
 		console.log('MongoDB connection succeed')
 		const PORT = process.env.PORT ?? 3003
